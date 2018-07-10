a23a84d1f9e4ff7b8724eccc459a521ed1b345d0
jetzable <gjetzable@gmail.com>
2018-07-10 08:18:33 -0500

    Agregando subtopics a objeto de estudiante

--- src/js/data.js
+++ src/js/data.js
@@ -27,6 +27,7 @@ window.computeStudentsStats = (laboratoria) => {
           statusEstudiante = 'average'; // Indicamos que esta en la media
         };
         const topics = Object.keys(student.progreso.temas);
+        let studentSubtopic;
         for (topic of topics) {
           // La siguiente linea añade los temas como nuevas propiedades del objeto topics y les da como valor que sean un objeto
           let newProperty = Object.defineProperty(student.progreso.temas, topic, { writable: true });
@@ -36,7 +37,8 @@ window.computeStudentsStats = (laboratoria) => {
             valuesTopicsEstudiante[i].completedPercentage = valuesTopicsEstudiante[i].porcentajeCompletado;
             let topicProgress = (valuesTopicsEstudiante[i].duracionTemaCompletado * 100) / valuesTopicsEstudiante[i].duracionTema;
             valuesTopicsEstudiante[i].percentageDuration = Math.round(topicProgress);
-          };
+            valuesTopicsEstudiante[i].subtopics = valuesTopicsEstudiante[i].subtemas;
+          }
         };
         studentsArray.push({
           'name': nombreEstudiante,
@@ -203,7 +205,7 @@ window.sortStudents = (students, orderBy, orderDirection) => {
       }
     }
   }
- 
+
   return orderedStudents;
 },
 
